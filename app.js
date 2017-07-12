ac3aac72598f803cb5c9c9df6c52a57928ba9f51
Kesha Seeley <keshalynne@gmail.com>
2017-07-12 14:55:53 -0700

    code clean up

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ firebase.initializeApp(config);
 var messageAppReference = firebase.database();
 
 $(document).ready(function() {
-
   // on click in nav, toggle class="hidden"
   $('#navLocations').click(function(event){
       event.preventDefault();
@@ -77,7 +76,7 @@ function latLngSearch(event) {
   var $lng = $('#lng').val();
   var newSource = 'https://embed.windy.com/embed2.html?lat=' + $lat + '&lon=' + $lng + '&zoom=3&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C';
   $('#weatherMap').attr('src', newSource);
-  console.log(newSource);
+  // console.log(newSource);
 }
 
 function getPosts() {
@@ -158,9 +157,8 @@ function deleteMessage(id){
 
   }
 
-// Google Map: Marker Cluster
+// Google Map: Markers
 function initMap() {
-
   var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 3,
       center: {lat: -28.024, lng: 140.887},
@@ -188,8 +186,14 @@ function initMap() {
 var locations = [
   {lat: -28.001744, lng: 153.42844}, // gold coast, australia
   {lat: 21.631201, lng: -158.069305}, // oahu, hawaii
-  // {lat: 54.479142, lng: -8.277934} // bundoran beach, ireland
   {lat: 3.945651, lng: 108.142868}, // mentawai, indonesia
+  {lat: 9.8708300, lng: 126.0511100}, // siargao island, philippines
+  {lat: -14.270284, lng: -170.704681}, // samoa
+  {lat: -37.8, lng: 174.883333}, // manu bay, raglan, new zealand
+  {lat: 18.24306, lng: 109.505}, // riyuewan, sanya, hainan, china
+  {lat: -17.8667, lng: 177.2000}, // tavarua island, fiji
+  {lat: 26.301667, lng: 127.909167} // white beach, okinawa, japan
+  // {lat: 54.479142, lng: -8.277934} // bundoran beach, ireland
   // {lat: ‎-34.033333, lng: 24.916668}, // j's bay, south africa
   // {lat: ‎-12.046374, lng: -77.042793}, // lima, peru
   // {lat: 28.358744, lng: -14.053676}, // fuerteventura, canary islands, spain
@@ -197,18 +201,12 @@ var locations = [
   // {lat: 43.676891, lng: -1.424724}, // hossegor, france
   // {lat: 13.48833, lng: -89.32222}, // la libertad, el salvador
   // {lat: ‎9.665231, lng: ‎-82.782066}, // playa naranjo, costa rica
-  {lat: 9.8708300, lng: 126.0511100}, // siargao island, philippines
   // {lat: -27.646620, lng: -48.670361}, // santa catarina, brazil
   // {lat: 1.924992, lng: ‎73.399658}, // north male, maldives
-  {lat: -14.270284, lng: -170.704681}, // samoa
   // {lat: -1.241667, lng: -78.619720}, // montanita, ecuador
-  {lat: -37.8, lng: 174.883333}, // manu bay, raglan, new zealand
-  {lat: 18.24306, lng: 109.505}, // riyuewan, sanya, hainan, china
-  {lat: -17.8667, lng: 177.2000}, // tavarua island, fiji
   // {lat: 23.614328, lng: 58.545284}, // oman
   // {lat: ‎50.503632, lng: -4.652498}, // watergate bay, cornwall, england
   // {lat: 18.340216, lng: -67.250015}, // rincon, puerto rico
-  {lat: 26.301667, lng: 127.909167} // white beach, okinawa, japan
   // {lat: 43.47664, lng: -1.51346}, // anglet, france
   // {lat: 49.152434, lng: -125.902493} // tofino, vancouver island, canada
 ]
